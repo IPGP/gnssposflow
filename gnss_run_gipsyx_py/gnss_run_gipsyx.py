3b9a429aa958684f16abd9cfb783cf99f5b07116
Pierre Sakic <sakic@ipgp.fr>
2026-08-24 11:56:27 +0200

    remove redundant directory creation for GipsyX results

--- gnss_run_gipsyx_py/gnss_run_gipsyx.py
+++ gnss_run_gipsyx_py/gnss_run_gipsyx.py
@@ -351,8 +351,8 @@ def try_orbits(config, opts, fid, ymd, rinex, tmpdir, gipsyres, log_path, verbos
         for old in glob.glob(f"{gipsyres}.*"):
             os.remove(old)
 
-        if not os.path.isdir(os.path.dirname(gipsyres)):
-            os.makedirs(os.path.dirname(gipsyres), exist_ok=True)
+        #if not os.path.isdir(os.path.dirname(gipsyres)):
+        #    os.makedirs(os.path.dirname(gipsyres), exist_ok=True)
 
         if not nforb_loop and not cm2cf:
             write_gipsyres_simple(tdp, gipsyres)
